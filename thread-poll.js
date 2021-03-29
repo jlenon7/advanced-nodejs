ea30c65a2b38a343a7791ffbcb93f688bee73f70
João Lenon <lenonsec7@gmail.com>
2021-03-29 15:15:15 -0300

    feat: Add comments

--- thread-poll.js
+++ thread-poll.js
@@ -7,8 +7,13 @@ function logHashTime() {
   })
 }
 
-logHashTime()
-logHashTime()
-logHashTime()
-logHashTime()
-logHashTime()
+// Libuv runs in 4 threads when the code needs more performance
+
+logHashTime() // 1
+logHashTime() // 2
+logHashTime() // 3
+logHashTime() // 4
+
+// Libuv will wait the 1 thread be free to run the other code
+
+logHashTime() // 5
